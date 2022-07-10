232abc348668eefec79955234a155a12e9c8adea
Michael Dickey <54130818+michaelfdickey@users.noreply.github.com>
2022-07-10 19:16:03 +0000

    api key working properly

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 """
@@ -9,14 +10,29 @@ send email to recipient
 news comes from https://newsapi.org/
 """
 
+# reference codespaces secret
+api_key = os.getenv('APIKEY')
+#print(api_key)
+
 #Define the search parameters
-def get_news(topic, from_date, to_date, language='en', api_key='get from secret'):
+def get_news(topic, from_date, to_date, language='en', api_key=api_key):
+    """
+    gets news results from newsapi.org
+    """
     url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key}'
+    #print("url is: ", url)
     request_result = requests.get(url)
     content = request_result.json()
+    #print(content)
+    
     articles = content['articles']
-    for article in articles:
-        print("> ", article['title'])   
+    
+    #for article in articles:
+    #    print("> ", article['title'])   
+
+    for i in range(len(content['articles'])):
+        print('> ', content['articles'][i]['title'])
         
-topic_search_result = get_news(topic='covid china', from_date='2022-6-27', to_date='2022-6-28')
+get_news(topic='covid china', from_date='2022-6-27', to_date='2022-6-28')
 #print(topic_search_result)
+
