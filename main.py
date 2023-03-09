630b7b81ea5b74ee4904439aff53a1d51d45c4b9
Michael Dickey <54130818+michaelfdickey@users.noreply.github.com>
2023-03-09 02:25:47 +0000

    added email capacity

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 from datetime import timedelta
 from datetime import date
 from datetime import datetime 
+import yagmail
+
 
 """
 v 1.0
@@ -17,28 +19,42 @@ news comes from https://newsapi.org/
 # reference codespaces secret
 api_key = os.getenv('APIKEY')
 api_key_weather = os.getenv('API_KEY_WEATHER')
+email_password = os.getenv('GMAIL_APP_PWD')
 #print(api_key)
 #print(api_key_weather)
+#print(email_password)
+
+# email info
+sender = 'mfd.oldphones@gmail.com'
+receiver = 'matus1976@gmail.com'
+subject = 'my dail news'
+content = ''
 
 #Define the search parameters
-def get_news(topic, from_date, to_date, language='en', api_key=api_key):
+def get_news(topic, from_date, to_date, language='en', api_key=api_key, content=content):
     """
     gets news results from newsapi.org
     """
     url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key}'
     #print("url is: ", url)
     request_result = requests.get(url)
-    content = request_result.json()
+    content_news = request_result.json()
     #print(content)
     
-    articles = content['articles']
+    articles = content_news['articles']
     
+    #print("content is: ", content)
     for article in articles:
         print("> ", article['title'])
+        content = content + "> " + article['title'] + "\n"
         print("   ", article['url'])
+        content = content + "  " + article['url'] + "\n"
+    #print("content is now: ", content)
 
     #for i in range(len(content['articles'])):
     #    print('> ', content['articles'][i]['title'])
+
+    return content
         
 def get_dates():
     today = date.today()
@@ -77,7 +93,7 @@ weather_content = get_weather(api_key_weather)
 forecast = get_forecast(api_key_weather)
 from_and_to_dates = get_dates()
 
-topics = ['covid china', 'nuclear energy', 'spacex', 'james webb', 'nuclear fusion', 'inflation', 'real estate', 'sp 500', 'blue origin']
+topics = ['covid china', 'nuclear energy', 'spacex', 'james webb', 'nuclear fusion', 'inflation', 'real estate', 'sp 500', 'blue origin', 'seattle']
 
 #build temperature display lists
 todays_temps = []
@@ -99,20 +115,46 @@ for n in range(8):
     #print(forecast_hour)
     forecast_hours.append(forecast_hour)
 
+### display results  and build conent ###
 
 print(" ")
+
 print("Daily News Delivery for", from_and_to_dates[1])
+content = "Daily News Delivery for " + from_and_to_dates[1]
+
 print(" ")
+content = content + "\n\n"
+
 print("The current weather for ", (weather_content['name']), "is: ", weather_content['weather']
       [0]['description'], " and a temp of ", weather_content['main']['temp'], " degrees.")
+content = content + "The current weather for " + weather_content['name'] + " is: " + weather_content['weather'][0]['description'] + " and a temp of " + str(weather_content['main']['temp']) + " degrees."
+
 print(" ")
+content = content + "\n\n"
+
 for n in range(8):
     print(forecast_hours[n], " ", todays_temps[n], " degF, ", todays_weather[n])
+    content = content + forecast_hours[n] + " " + str(todays_temps[n]) + " degF, " + todays_weather[n] + "\n"
     #print(" ")
+
 print(" ")
+content = content + "\n\n"
 
 for topic in topics:
     print(topic)
-    get_news(topic=topic, from_date=from_and_to_dates[1], to_date=from_and_to_dates[0])
+    content = content + topic + "\n"
+    content = get_news(topic=topic, from_date=from_and_to_dates[1], to_date=from_and_to_dates[0], content=content)
+    content = content + "\n"
 
 
+### create email ###
+subject = 'my dail news for ' + from_and_to_dates[1]
+print("subject will be: ", subject)
+
+### check contents ###
+print("content is: ", content)
+
+### send email ###
+yag = yagmail.SMTP(sender,email_password )                      # create an instance of the SMTP object isntance using the SMTP class
+yag.send(to=receiver, subject=subject, contents=content)
+print("email sent")
